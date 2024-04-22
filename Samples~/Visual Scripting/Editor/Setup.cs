64f7698cf141f101c53cd9761296181bec6e32c8
TITAN\ckilg <ckilgore@gmail.com>
2024-04-22 07:44:11 -0500

    Fixed VS setup

--- Samples~/Visual Scripting/Editor/Setup.cs
+++ Samples~/Visual Scripting/Editor/Setup.cs
@@ -1,29 +1,15 @@
 using System;
 using System.Collections.Generic;
 using ToolkitEngine.Inventory;
-using Unity.VisualScripting;
-using UnityEngine;
 using UnityEditor;
 
 namespace ToolkitEditor.Inventory.VisualScripting
 {
-    [InitializeOnLoad]
+	[InitializeOnLoad]
     public static class Setup
     {
         static Setup()
         {
-			bool dirty = false;
-			var config = BoltCore.Configuration;
-
-			var assemblyName = new LooseAssemblyName("ToolkitEngine.Inventory");
-			if (!config.assemblyOptions.Contains(assemblyName))
-			{
-				config.assemblyOptions.Add(assemblyName);
-				dirty = true;
-
-				Debug.LogFormat("Adding {0} to Visual Scripting assembly options.", assemblyName);
-			}
-
 			var types = new List<Type>()
 			{
 				typeof(ToolkitEngine.Inventory.Inventory),
@@ -41,24 +27,7 @@ namespace ToolkitEditor.Inventory.VisualScripting
 				typeof(Recipes),
 			};
 
-			foreach (var type in types)
-			{
-				if (!config.typeOptions.Contains(type))
-				{
-					config.typeOptions.Add(type);
-					dirty = true;
-
-					Debug.LogFormat("Adding {0} to Visual Scripting type options.", type.FullName);
-				}
-			}
-
-			if (dirty)
-			{
-				var metadata = config.GetMetadata(nameof(config.typeOptions));
-				metadata.Save();
-				Codebase.UpdateSettings();
-				UnitBase.Rebuild();
-			}
+			ToolkitEditor.VisualScripting.Setup.Initialize("ToolkitEngine.Inventory", types);
 		}
     }
 }
