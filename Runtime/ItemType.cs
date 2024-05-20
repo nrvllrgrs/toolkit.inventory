144cef5dc49e45e9287ea32e3ae35cfe0d31a4d8
TITAN\ckilg <ckilgore@gmail.com>
2024-05-19 19:44:30 -0500

    Exposed ItemType for edtor type

--- Runtime/ItemType.cs
+++ Runtime/ItemType.cs
@@ -90,7 +90,7 @@ namespace ToolkitEngine.Inventory
 
         public string id => m_id;
         public ItemType parent => m_parent;
-        public new string name => m_name;
+        public new string name { get => m_name; set => m_name = value; }
         public string description => m_description;
         public Sprite icon => m_icon;
         public Color color => m_color;
