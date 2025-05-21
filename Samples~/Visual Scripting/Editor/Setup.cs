a6549d883e477b4aec66c435de86e419e7f3ab24
TITAN\ckilg <ckilgore@gmail.com>
2025-05-21 16:03:45 -0500

    Added BaseEvent unit to namespace Creatde assembly definition for Inventory.VisualScripting

--- Samples~/Visual Scripting/Editor/Setup.cs
+++ Samples~/Visual Scripting/Editor/Setup.cs
@@ -1,15 +1,29 @@
 using System;
 using System.Collections.Generic;
 using ToolkitEngine.Inventory;
+using Unity.VisualScripting;
+using UnityEngine;
 using UnityEditor;
 
 namespace ToolkitEditor.Inventory.VisualScripting
 {
-	[InitializeOnLoad]
+    [InitializeOnLoad]
     public static class Setup
     {
         static Setup()
         {
+			bool dirty = false;
+			var config = BoltCore.Configuration;
+
+			var assemblyName = new LooseAssemblyName("ToolkitEngine.Inventory");
+			if (!config.assemblyOptions.Contains(assemblyName))
+			{
+				config.assemblyOptions.Add(assemblyName);
+				dirty = true;
+
+				Debug.LogFormat("Adding {0} to Visual Scripting assembly options.", assemblyName);
+			}
+
 			var types = new List<Type>()
 			{
 				typeof(InventoryList),
@@ -28,7 +42,24 @@ namespace ToolkitEditor.Inventory.VisualScripting
 				typeof(Recipes),
 			};
 
-			ToolkitEditor.VisualScripting.Setup.Initialize("ToolkitEngine.Inventory", types);
+			foreach (var type in types)
+			{
+				if (!config.typeOptions.Contains(type))
+				{
+					config.typeOptions.Add(type);
+					dirty = true;
+
+					Debug.LogFormat("Adding {0} to Visual Scripting type options.", type.FullName);
+				}
+			}
+
+			if (dirty)
+			{
+				var metadata = config.GetMetadata(nameof(config.typeOptions));
+				metadata.Save();
+				Codebase.UpdateSettings();
+				UnitBase.Rebuild();
+			}
 		}
     }
 }
